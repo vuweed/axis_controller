ddfa9beb21608359826a4efd73cb997cdad4753f
vuweed <halucminhvu2000@gmail.com>
2023-06-20 17:45:21 +0700

    add lib axis_driver

---
+++ Core/Inc/axis_driver.h
@@ -0,0 +1,27 @@
+/*
+ * axis_driver.h
+ *
+ *  Created on: Jun 20, 2023
+ *      Author: ADMIN
+ */
+
+#ifndef INC_AXIS_DRIVER_H_
+#define INC_AXIS_DRIVER_H_
+
+
+typedef struct
+{
+    uint16_t pwm;
+    uint16_t angle;
+    uint32_t desired_value;
+}M_axis_t;
+
+
+
+
+
+
+
+
+
+#endif /* INC_AXIS_DRIVER_H_ */
---
+++ Core/Src/axis_driver.c
@@ -0,0 +1,10 @@
+/*
+ * axis_driver.c
+ *
+ *  Created on: Jun 20, 2023
+ *      Author: ADMIN
+ */
+
+#include "axis_driver.h"
+
+
